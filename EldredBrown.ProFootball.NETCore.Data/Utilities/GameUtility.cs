a240253c24aa195f8a5cc83a90f81510e3016590
eldredbrown <eldred.brown@outlook.com>
2021-02-18 23:03:28 -0800

    Removed DecideWinnerAndLoser method from GameUtility class

--- EldredBrown.ProFootball.NETCore.Data/Utilities/GameUtility.cs
+++ EldredBrown.ProFootball.NETCore.Data/Utilities/GameUtility.cs
@@ -5,33 +5,6 @@ namespace EldredBrown.ProFootball.NETCore.Data.Utilities
     public class GameUtility : IGameUtility
     {
         /// <summary>
-        /// Decides the winner and loser of the specified <see cref="Game"/> entity.
-        /// </summary>
-        /// <param name="game">The <see cref="Game"/> entity from which data will be accessed for modification.</param>
-        public void DecideWinnerAndLoser(Game game)
-        {
-            if (game.GuestScore > game.HostScore)
-            {
-                game.WinnerName = game.GuestName;
-                game.WinnerScore = game.GuestScore;
-                game.LoserName = game.HostName;
-                game.LoserScore = game.HostScore;
-            }
-            else if (game.HostScore > game.GuestScore)
-            {
-                game.WinnerName = game.HostName;
-                game.WinnerScore = game.HostScore;
-                game.LoserName = game.GuestName;
-                game.LoserScore = game.GuestScore;
-            }
-            else
-            {
-                game.WinnerName = null;
-                game.LoserName = null;
-            }
-        }
-
-        /// <summary>
         /// Edits a <see cref="Game"/> entity with data from another <see cref="Game"/> entity.
         /// </summary>
         /// <param name="destGame">The <see cref="Game"/> entity to which data will be copied.</param>
--- EldredBrown.ProFootball.NETCore.Data/Utilities/IGameUtility.cs
+++ EldredBrown.ProFootball.NETCore.Data/Utilities/IGameUtility.cs
@@ -5,12 +5,6 @@ namespace EldredBrown.ProFootball.NETCore.Data.Utilities
     public interface IGameUtility
     {
         /// <summary>
-        /// Decides the winner and loser of the specified <see cref="Game"/> entity.
-        /// </summary>
-        /// <param name="game">The <see cref="Game"/> entity from which data will be accessed for modification.</param>
-        void DecideWinnerAndLoser(Game game);
-
-        /// <summary>
         /// Edits a <see cref="Game"/> entity with data from another <see cref="Game"/> entity.
         /// </summary>
         /// <param name="destGame">The <see cref="Game"/> entity to which data will be copied.</param>
