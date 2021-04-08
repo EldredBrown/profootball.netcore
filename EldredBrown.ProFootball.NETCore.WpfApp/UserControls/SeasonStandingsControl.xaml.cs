44ca253e41a8057c6092a24607a47aecfdccd718
eldredbrown <eldred.brown@outlook.com>
2021-04-07 22:44:50 -0700

    WpfApp: Added SeasonStandingsControl UX functionality

---
+++ EldredBrown.ProFootball.NETCore.WpfApp/UserControls/SeasonStandingsControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using EldredBrown.ProFootball.NETCore.WpfApp.ViewModels;
+
+namespace EldredBrown.ProFootball.NETCore.WpfApp.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SeasonStandingsControl.xaml
+    /// </summary>
+    public partial class SeasonStandingsControl : UserControl
+    {
+        public SeasonStandingsControl()
+        {
+            InitializeComponent();
+
+            DataContext = new SeasonStandingsControlViewModel();
+        }
+    }
+}
---
+++ EldredBrown.ProFootball.NETCore.WpfApp/ViewModels/SeasonStandingsControlViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using EldredBrown.ProFootball.NETCore.Data.Entities;
+
+namespace EldredBrown.ProFootball.NETCore.WpfApp.ViewModels
+{
+    public class SeasonStandingsControlViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Gets or sets this control's standings collection.
+        /// </summary>
+        private ReadOnlyCollection<SeasonTeamStanding> _standings;
+        public ReadOnlyCollection<SeasonTeamStanding> Standings
+        {
+            get
+            {
+                return _standings;
+            }
+            set
+            {
+                if (value != _standings)
+                {
+                    _standings = value;
+                    OnPropertyChanged("Standings");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Loads the control.
+        /// </summary>
+        private DelegateCommand _viewStandingsCommand;
+        public DelegateCommand ViewStandingsCommand
+        {
+            get
+            {
+                if (_viewStandingsCommand is null)
+                {
+                    _viewStandingsCommand = new DelegateCommand(param => ViewStandings());
+                }
+                return _viewStandingsCommand;
+            }
+        }
+        private void ViewStandings()
+        {
+            Standings = new ReadOnlyCollection<SeasonTeamStanding>(
+                new List<SeasonTeamStanding>
+                {
+                    new SeasonTeamStanding { Team = "Team 1", Wins = 2, Losses = 0 },
+                    new SeasonTeamStanding { Team = "Team 2", Wins = 1, Losses = 1 },
+                    new SeasonTeamStanding { Team = "Team 3", Wins = 0, Losses = 2 }
+                });
+        }
+    }
+}
